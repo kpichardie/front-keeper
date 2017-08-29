3313f2e5f3a6cfc29f6358ea1481cc8d9b91ebfa
kevin.pichardie <kevin.pichardie@enovance.com>
2017-08-29 11:07:04 +0200

    Change allowed host for docker

--- frontkeeper/frontkeeper/settings.py
+++ frontkeeper/frontkeeper/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'change-me!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
